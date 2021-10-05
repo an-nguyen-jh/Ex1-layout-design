978a2f00961707fd863ae18e7e0939b47357dc2d
Nguyen Hong An <hongan150699@gmail.com>
2021-10-05 18:22:04 +0700

    [Fix] add css variable & add standard padding for small screen and big screen

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function toggle(id) {
   const collapseNav = document.getElementById("collapse-nav");
-
-  if (collapseNav.classList.contains("toggle")) {
-    collapseNav.classList.remove("toggle");
-  } else {
-    collapseNav.classList.add("toggle");
-  }
+  collapseNav.classList.toggle("toggle");
+  // console.log(collapseNav.style);
+  // if (collapseNav.style.opacity === "0") {
+  //   collapseNav.style.opacity = "1";
+  //   collapseNav.style.height = "calc(100vh -60px)";
+  // } else {
+  //   collapseNav.style.opacity = "0";
+  //   collapseNav.style.height = "0xp";
+  // }
 }
 
 const toggleBtn = document.getElementById("collapse-btn");
-const infoBtn = document.getElementById("collapse-info");
+//const infoBtn = document.getElementById("collapse-info");
 
 toggleBtn.addEventListener("click", () => {
   toggle("collapse-nav");
