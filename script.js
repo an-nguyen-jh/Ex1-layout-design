e0e9a02d5a43e331c23b632f38b2a9af97fe2fc5
Nguyen Hong An <hongan150699@gmail.com>
2021-10-07 08:51:01 +0700

    [Fearture] rotate homepage logo

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ function toggle(id) {
 function shinkNavbarWhenScroll() {
   //determine event when webpage scroll
   const navbar = document.getElementById("navbar");
+  const verticalLogo = document.querySelector(
+    ".navbar__homepage-logo--vertical"
+  );
+  console.log(verticalLogo.style);
   //console.log(window.innerHeight, window.innerWidth);
   //set the break point when the navbar change background
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navbar.style.backgroundColor = "#323339";
-    // navbar.style.lineHeight = "60px";
+    verticalLogo.style.top = "0px";
+    verticalLogo.style.transform = "rotate(0deg)";
   } else {
     navbar.style.backgroundColor = "transparent";
-    // navbar.style.lineHeight = "80px";
+    verticalLogo.style.top = "60px";
+    verticalLogo.style.transform = "rotate(90deg) translate(50%, -50%)";
   }
 }
 
