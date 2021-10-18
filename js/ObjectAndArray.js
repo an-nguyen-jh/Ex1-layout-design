2750576b193fea38caba8afec387ca30dcad945e
Nguyen Hong An <hongan150699@gmail.com>
2021-10-18 17:58:52 +0700

    [JS] [Obj&Array] create decision tree with add, edit, delete, inorder traversal method

--- js/ObjectAndArray.js
+++ js/ObjectAndArray.js
@@ -97,3 +97,186 @@ function findMostRepetitionElement(array) {
 }
 
 // console.log(findMostRepetitionElement([]));
+//==============================================Exercise 3===========================================================
+// Ex 3: Create a decision question tree,
+// the answer of the previous question will affect the appearance of the following question.
+// Requirements: Easily add, edit or delete to the questionnaire at low cost.
+/* Node structor: question :{ một câu hỏi
+  specifyFlag: {} cờ để phân biệt các loại câu hỏi phù hợp với câu trả lời nào trong node trước
+  content: Nội dung câu hỏi 
+  answers: [ danh sách các câu trả lời
+    specifyFlag: {} cờ để phân biệt các loại câu hỏi phù hợp với câu trả lời nào trong node trước
+    content: Nội dung câu trả lời
+    next: con trỏ trỏ tới câu hõi tiếp theo
+  ]
+} */
+
+const fs = require("fs");
+class DecisionNode {
+  constructor(question, answers) {
+    const questionObj = {
+      specifyFlag: question.code,
+      content: question.content,
+    };
+    const answersObj = [];
+    for (let i = 0; i < answers.length; i++) {
+      const temp = {
+        specifyFlag: questionObj.specifyFlag + i,
+        content: answers[i],
+        next: null,
+      };
+      answersObj[i] = temp;
+    }
+    this.question = questionObj;
+    this.answers = answersObj;
+  }
+
+  preOrder(root) {
+    if (root === null) {
+      return;
+    }
+    console.log(root.question);
+    console.log(root.answers);
+    for (const answer of root.answers) {
+      this.preOrder(answer.next);
+    }
+  }
+
+  add(root, newNode, level = 1) {
+    if (root === null) {
+      return newNode;
+    }
+    for (const answer of root.answers) {
+      //find right path of answer to add new node
+      if (answer.specifyFlag[level] === newNode.question.specifyFlag[level]) {
+        //question.specifyFlag.startWith(newNode.answer.specifyFlag)
+        answer.next = this.add(answer.next, newNode, ++level);
+        break;
+      }
+    }
+    return root;
+  }
+
+  edit(root, editedNode) {
+    let head = root;
+    while (head !== null) {
+      const preNode = head;
+      for (const answer of head.answers) {
+        if (editedNode.question.specifyFlag.startsWith(answer.specifyFlag)) {
+          if (answer.specifyFlag === editedNode.question.specifyFlag) {
+            //if answer.next does not contain any question
+            const oldNode = answer.next;
+            if (oldNode === null) {
+              answer.next = editedNode;
+            }
+            //attach every sub question in to new Node
+            console.log(editedNode.answers.length);
+            for (let i = 0; i < editedNode.answers.length; i++) {
+              if (
+                oldNode.answers[i] &&
+                oldNode.answers[i].specifyFlag ===
+                  editedNode.answers[i].specifyFlag
+              ) {
+                editedNode.answers[i].next = oldNode.answers[i].next;
+              }
+            }
+            // console.log(oldNode, editedNode);
+            // attach new edited node to tree
+            answer.next = editedNode;
+          } else {
+            head = answer.next;
+          }
+          break;
+        }
+      }
+      //check of specifyFlag is valid
+      if (head === preNode) {
+        break;
+      }
+    }
+  }
+
+  delete(root, specifyFlag) {
+    let head = root;
+    while (head !== null) {
+      const preNode = head;
+      for (const answer of head.answers) {
+        if (specifyFlag.startsWith(answer.specifyFlag)) {
+          if (answer.specifyFlag === specifyFlag) {
+            answer.next = null;
+          } else {
+            head = answer.next;
+          }
+          break;
+        }
+      }
+      if (head === preNode) {
+        break;
+      }
+    }
+  }
+}
+
+function writeFileISON(obj) {
+  return fs.writeFile("test.json", JSON.stringify(obj), function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("Data written successfully!");
+  });
+}
+
+function testDecisionTree() {
+  let decisionNode = new DecisionNode(
+    {
+      code: "0",
+      content:
+        "Let's start learning path, please, self assessment your javascript's level",
+    },
+    ["Know nothing", "Beginner", "Intermediate", "Advanced"]
+  );
+
+  const nothing1 = new DecisionNode(
+    { code: "00", content: "Let's start our basic learning path" },
+    ["Ok", "Cancel", "Explain basic learning path ?"]
+  );
+  const nothing2 = new DecisionNode(
+    { code: "000", content: "Did you pass basic learning path?" },
+    ["No, I didn't", "I am finished"]
+  );
+  const beginner1 = new DecisionNode(
+    { code: "01", content: "Would you start basic learning path?" },
+    ["OK", "NO", "Let switch to Intermediate learning path"]
+  );
+  const beginner2 = new DecisionNode(
+    {
+      code: "011",
+      content: "Would you like to take a small test to check your knowledge?",
+    },
+    ["No", "Yes", "Why i have to?"]
+  );
+
+  console.log("Add New Node ===========================================");
+  decisionNode.add(decisionNode, nothing1);
+  decisionNode.add(decisionNode, nothing2);
+  decisionNode.add(decisionNode, beginner1);
+  decisionNode.add(decisionNode, beginner2);
+  //decisionNode.preOrder(decisionNode);
+  //writeFileISON(decisionNode);
+  // console.log("Delete Node ===========================================");
+  const editedNode = new DecisionNode(
+    {
+      code: "01",
+      content: "Would you start beginner learning path?",
+    },
+    ["OK", "No"]
+  );
+
+  decisionNode.edit(decisionNode, editedNode);
+  //writeFileISON(decisionNode);
+  // console.log("Delete Node ===========================================");
+  // decisionNode.delete(decisionNode, "01");
+  // writeFileISON(decisionNode);
+}
+
+testDecisionTree();
--- js/RegExp.js
+++ js/RegExp.js
@@ -5,13 +5,17 @@ const regExpPrototype = {
   ),
   //local-part
   //[a-z0-9!#$%&'*+/=?^_`{|}~-]: các ký tự được chấp nhận khi đặt tên email
-  //(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+):
-  // * : trùng 0 or nhiều đoạn ký mã reg trước đó
+  //(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*: các ký tự được chấp nhận khi đặt tên email bất đầu với dấu chấm
+  // * : trùng 0 or nhiều đoạn mã reg trước đó
   // @: trùng ký tự @
   //\. : trùng với ký tự .
   //[a-z0-9-]: trùng với các ký tự từ a-z, 0-9 và ký tư "-"
   // ?: trùng với 0 or 1 đoạn mã reg trước đó
-  password: new RegExp(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, "gm"),
+  password: new RegExp(/[0-9]+\W+[A-Z]+.{8,}/, "gm"),
+  //[0-9]+; ít nhất 1 số
+  // \W+: ít nhất 1 ký tự đặc biệt
+  //[A-Z]+ Chứa ít nhất 1 ký tự in hoa
+  //{8,}: chứa ít nhất 8 ký tự
 };
 
 function checkMatchedStringByRegExp(str, regExpType) {
