f76af011128ae7520ce3705ac80bb202f823a0c1
Nguyen Hong An <hongan150699@gmail.com>
2021-10-29 13:46:15 +0700

    [FullWeb] [Fix] change behaviour of  scroll container when it is shorter than screen width and add scroll directly to  next section in the section title

--- Pixture/script.js
+++ Pixture/script.js
@@ -4,13 +4,17 @@ const pictureNav = document.getElementById("picture-nav");
 const articleNav = document.getElementById("article-nav");
 const pictures = document.querySelector(".picture-section .scroll-container");
 const articles = document.querySelector(".article-section .scroll-container");
-const header = document.getElementsByClassName("header")[0];
+const scrollSectionList = document.querySelectorAll(
+  ".section-heading__content__chevron-btn"
+);
 
 const PICTURES_SCROLL_WIDTH = pictures.scrollWidth;
 const ARTICLES_SCROLL_WIDTH = articles.scrollWidth;
 const SCROLL_CONTAINER_WIDTH =
-  document.querySelector(".scroll-group").offsetWidth - pictureNav.offsetWidth;
+  document.querySelector(".scroll-group").offsetWidth;
 const CARD_WIDTH = document.querySelector(".scroll__card").offsetWidth;
+
+const navigationList = ["sub-masthead", "credit-img"];
 
 function toggleMenu() {
   const collapseNav = document.getElementById("header-menu");
@@ -18,6 +22,7 @@ function toggleMenu() {
   header.classList.toggle("toggle");
 }
 
+// add scroll behavior to scroll button
 function scrollCardList(scrollElm, scrollWidth) {
   let translateLen = CARD_WIDTH;
   if (translateLen > scrollWidth - SCROLL_CONTAINER_WIDTH) {
@@ -27,6 +32,7 @@ function scrollCardList(scrollElm, scrollWidth) {
   scrollElm.scrollLeft = Math.floor(scrollElm.scrollLeft - translateLen);
 }
 
+//handle dragg to scroll in scroll container
 function handleDragStart(e) {
   e.preventDefault();
   this.isDown = true;
@@ -59,13 +65,39 @@ pictureNav.addEventListener("click", function () {
 articleNav.addEventListener("click", function () {
   scrollCardList(articles, ARTICLES_SCROLL_WIDTH);
 });
-
-articles.addEventListener("mousedown", handleDragStart);
-articles.addEventListener("mousemove", handleDragMove);
-articles.addEventListener("mouseup", handleDragEnd);
-articles.addEventListener("mouseleave", handleDragEnd);
-
-pictures.addEventListener("mousedown", handleDragStart);
-pictures.addEventListener("mousemove", handleDragMove);
-pictures.addEventListener("mouseup", handleDragEnd);
-pictures.addEventListener("mouseleave", handleDragEnd);
+//if pictures or articles width smaller than container width
+// set justify-content center & remove navigation
+if (PICTURES_SCROLL_WIDTH <= SCROLL_CONTAINER_WIDTH) {
+  pictures.style.justifyContent = "center";
+  pictures.style.cursor = "auto";
+  pictureNav.style.display = "none";
+} else {
+  pictures.addEventListener("mousedown", handleDragStart);
+  pictures.addEventListener("mousemove", handleDragMove);
+  pictures.addEventListener("mouseup", handleDragEnd);
+  pictures.addEventListener("mouseleave", handleDragEnd);
+}
+if (ARTICLES_SCROLL_WIDTH <= SCROLL_CONTAINER_WIDTH) {
+  articles.style.justifyContent = "center";
+  articleNav.style.display = "none";
+  pictures.style.cursor = "auto";
+} else {
+  articles.addEventListener("mousedown", handleDragStart);
+  articles.addEventListener("mousemove", handleDragMove);
+  articles.addEventListener("mouseup", handleDragEnd);
+  articles.addEventListener("mouseleave", handleDragEnd);
+}
+console.log(scrollSectionList);
+scrollSectionList.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    //support recently
+    // document.getElementById(navigationList[i]).scrollIntoView({
+    //   behavior: "smooth",
+    // });
+    const offsetTop = document.getElementById(navigationList[i]).offsetTop;
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
